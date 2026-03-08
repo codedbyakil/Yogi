f47b4e672636d05d056ff7298b307016cc7e1933
ᴀᴋɪʟ ᴏsᴄᴀɴ <akilaskan@gmail.com>
2026-03-08 12:08:40 +0530

    Update check_streams.py

--- check_streams.py
+++ check_streams.py
@@ -1,38 +1,27 @@
-import requests
 import sys
 
 source = sys.argv[1]
 output = sys.argv[2]
 
-timeout = 8
-
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Range": "bytes=0-1024"
-}
-
 with open(source, "r", encoding="utf-8", errors="ignore") as f:
     lines = f.readlines()
 
 out = ["#EXTM3U\n"]
+block = []
 
-i = 0
-while i < len(lines):
-    if lines[i].startswith("#EXTINF"):
-        info = lines[i]
-        url = lines[i+1].strip()
+for line in lines:
+    if line.startswith("#EXTINF"):
+        if block:
+            out.extend(block)
+            block = []
+        block.append(line)
 
-        try:
-            r = requests.get(url, headers=headers, timeout=timeout, stream=True)
-            if r.status_code in [200, 206]:
-                out.append(info)
-                out.append(url + "\n")
-        except:
-            pass
+    elif block:
+        block.append(line)
 
-        i += 2
-    else:
-        i += 1
+# add last block
+if block:
+    out.extend(block)
 
 with open(output, "w", encoding="utf-8") as f:
     f.writelines(out)
